7882b37859c3b885545c2eabf5aa521fd4ad3f9f
maborosii <bonvovage@163.com>
2023-07-08 16:45:09 +0800

    add noice plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -190,9 +190,10 @@ local plugins = {
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {
-      -- add any options here
-    },
+    opts = function()
+      return require "custom.configs.noice"
+    end,
+    -- add any options here
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
